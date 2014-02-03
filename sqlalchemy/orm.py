55baf3b08c0d288711b33ea190ee425f70d1dcbb
Chan Shik Lim <chanshik@gmail.com>
2014-02-03 21:33:29 +0900

    Add some imports.

--- sqlalchemy/orm.py
+++ sqlalchemy/orm.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, Sequence
-from sqlalchemy import and_, or_
+from sqlalchemy import ForeignKey
+from sqlalchemy import and_, or_, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
 
 engine = create_engine('sqlite:///:memory:', echo=True)
 Base = declarative_base()
@@ -19,11 +22,25 @@ class User(Base):
     name = Column(String)
     fullname = Column(String)
     password = Column(String)
+    addresses = relationship("Address", order_by="Address.id", backref="user")
 
     def __repr__(self):
         return "<User(name='%s', fullname='%s', password='%s')>" % (
             self.name, self.fullname, self.password
         )
+
+
+class Address(Base):
+    __tablename__ = 'addresses'
+
+    id = Column(Integer, primary_key=True)
+    email_address = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    user = relationship('User', backref=backref('addresses', order_by=id))
+
+    def __repr__(self):
+        return "<Address(email_address='%s')>" % self.email_address
 
 
 Base.metadata.create_all(engine)
